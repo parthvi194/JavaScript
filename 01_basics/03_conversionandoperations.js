352a4b32c21d3e9242481c3c9f3499e1073e9ceb
Parthvi Modi <modiparthvi106@gmail.com>
2026-08-10 05:02:38 +0000

    Operatins updated

--- 01_basics/03_conversionandoperations.js
+++ 01_basics/03_conversionandoperations.js
@@ -1,3 +1,5 @@
+// ************* Conversions ****************
+
 let score = "parthvi"
 
 // console.log(typeof score);
@@ -24,5 +26,44 @@ let booleanislogged_in = Boolean(islogged_in)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************* Operations ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " parthvi" 
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
