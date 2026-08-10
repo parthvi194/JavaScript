1ef26bd02ffcfe38d178ff10398a5c529c88eacc
Parthvi Modi <modiparthvi106@gmail.com>
2026-08-10 04:37:10 +0000

    Update conversion and operations

--- 01_basics/03_conversionandoperations.js
+++ 01_basics/03_conversionandoperations.js
@@ -1,8 +1,28 @@
-let score = "33abc"
+let score = "parthvi"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valuein_number = Number(score)
-console.log(typeof valuein_number);
-console.log(valuein_number);
+// console.log(typeof valuein_number);
+// console.log(valuein_number);
+
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1;  false = 0
+
+let islogged_in = "parthvi"
+
+let booleanislogged_in = Boolean(islogged_in)
+// console.log(booleanislogged_in);
+
+// true = 1;  false = 0
+// "" = false
+// "parthvi" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
