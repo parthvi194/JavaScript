07d44b5c57dbc987f33d22788e4064bdab00d1d1
Parthvi Modi <modiparthvi106@gmail.com>
2026-08-11 06:15:22 +0000

    functions part-1 done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,40 @@
+function sayMyname(){
+    console.log("P");
+    console.log("A");
+    console.log("R");
+    console.log("T");
+    console.log("H");
+    console.log("V");
+    console.log("I");
+}
+
+// sayMyname()
+
+// function addTwonum(number1, number2){  // parameter passed
+//     console.log(number1 + number2);
+// }
+
+function addTwonum(number1, number2){  // parameter passed
+
+    // let result = number1 + number2
+    // return result                    // method-1
+
+    return number1 + number2            // method-2
+}
+
+const result = addTwonum(67, 66)        // arguments passed
+// console.log("Result:", result);
+
+
+function loginUserMessage(username = "you are") {
+    if(!username){
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Parthvi"))     // value passed (o/p -- Parthvi just logged in)
+// console.log(loginUserMessage(""))     // empty string passed (o/p --  just logged in)
+console.log(loginUserMessage())      // value not passed (o/p -- undefined just logged in)
+
