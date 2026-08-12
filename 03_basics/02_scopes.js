6f06a39a6deb8f9bb59420111d21d4888d38e0d8
Parthvi Modi <modiparthvi106@gmail.com>
2026-08-12 05:11:00 +0000

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,3 +1,5 @@
+// ******** global and local scope ********
+
 // let a = 10
 // const b = 20
 // var c = 30
@@ -7,9 +9,56 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner: ",a);
+    // console.log("Inner: ",a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// ******** nested scope ********
+
+function one() {
+    const username = "Parthvi"
+
+    function two(){
+        const website = "YouTube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username ="Parthvi"
+    if (username === "Parthvi") {
+        const website = " YouTube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ********* Interesting *********
+
+// console.log(addOne(5))
+function addOne(num) {
+    return num + 1
+}
+
+// console.log(addOne(5))
+
+
+
+
+const addTwo = function (num) {
+    return num + 2
+}
+
+// console.log(addTwo(5))
