630589a9ec4fd3ee9a8c39fe7509b7f8ded9199b
Parthvi Modi <modiparthvi106@gmail.com>
2026-08-13 07:09:49 +0000

    truthy and falsy value

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,10 +1,10 @@
 const userEmail = []  // "p@parthvi.ai"
 
-if (userEmail) {
-    console.log("Got user email");
-} else {
-    console.log("Don't have user email");
-}
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
 
 
 // falsy values -- false, 0, -0, BigInt 0n, null, "", undefined, NaN
@@ -17,6 +17,26 @@ if (userEmail) {
 
 const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
-}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
+
+// Nullish coalescing operator (??) -- null, undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTea = 100
+
+iceTea <= 80 ? console.log("less than 80"): console.log("more than 80");
+
