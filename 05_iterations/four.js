577c9d51cfebb426a19fa7912d500d692aa02194
Parthvi Modi <modiparthvi106@gmail.com>
2026-08-13 10:55:17 +0000

    object with forin loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+// const myObj = {
+//     js: 'JavaScript',
+//     cpp: 'C++',
+//     rb: 'Ruby',
+//     py: 'Python'
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`);   
+// }
+
+// const pg = ["js","rb","py","cpp"]
+
+// for (const key in pg) {
+//     console.log(pg[key]);
+    
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
